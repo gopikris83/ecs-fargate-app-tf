70f26a2d62f449d4842a4d9c3a7439661f2f5e4f
Gopi Krishna <gopikrishna.83@gmail.com>
2022-03-03 15:21:58 +0100

    User-agent changed

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, app
 from flask import render_template
-import requests, os
+import requests, os, json
 
 
 app = Flask(__name__)
@@ -8,9 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 def welcome_message():
     info = "Welcome to 2022!"
-    user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
-    r = requests.get("https://httpbin.org/headers", headers=user_agent)
-    return render_template('index.html', welcome_message=info, user_agent_info=r.text)
+    #user_agent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82 Safari/537.36'}
+    r = requests.utils.default_headers()
+    return render_template('index.html', welcome_message=info, user_agent_info=r['User-Agent'])
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
